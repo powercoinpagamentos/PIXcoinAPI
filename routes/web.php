e7e4fee34d5f7a48928ffd625776cf74ee426159
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2025-01-08 12:37:12 -0300

    Revert "feat: teste de redirecionamento"
    This reverts commit 9448f926c959f93f731238f3a4b7d21ad4707296.

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/frontend/{any?}', function () {
-    return redirect('http://127.0.0.1:3000/' . request()->path());
-})->where('any', '.*');
