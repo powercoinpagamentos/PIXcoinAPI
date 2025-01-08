9448f926c959f93f731238f3a4b7d21ad4707296
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2025-01-08 12:35:49 -0300

    feat: teste de redirecionamento

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/frontend/{any?}', function () {
+    return redirect('http://127.0.0.1:3000/' . request()->path());
+})->where('any', '.*');
