cddf045f1618ae0ea9a198e72651e351f9609b14
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2025-05-23 20:18:53 -0300

    Revert "Fecha conexões após processos em AdminLogin"
    This reverts commit de902ef9557b4701c8e733d7eb9ff3380b0982c6.

--- app/Actions/Admin/AdminLogin.php
+++ app/Actions/Admin/AdminLogin.php
@@ -6,7 +6,6 @@ use App\Models\Pessoa;
 use DateMalformedStringException;
 use DateTimeImmutable;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Lcobucci\JWT\Configuration;
 use Lcobucci\JWT\Signer\Hmac\Sha256;
@@ -21,40 +20,26 @@ readonly class AdminLogin
 
     public function run(): JsonResponse
     {
-        DB::beginTransaction();
-
-        try {
-            $user = $this->retrieveAdmin();
-
-            if (!$user || !Hash::check($this->password, $user->senha)) {
-                DB::rollBack();
-                DB::disconnect();
-                return response()->json(['error' => 'Email ou senha inválidos'], 400);
-            }
-
-            $this->updateLastAccess($user);
-
-            DB::commit();
-
-            return response()->json([
-                'email' => $user->email,
-                'id' => $user->id,
-                'name' => $user->nome,
-                'lastLogin' => $user->ultimo_acesso->setTimezone('America/Sao_Paulo')->format('Y-m-d\TH:i:sP'),
-                'token' => $this->generateJWT($user->id),
-                'type' => 'pessoa',
-                'key' => 'ADMIN'
-            ]);
-
-        } catch (RandomException $e) {
-            DB::rollBack();
-            return response()->json(['error' => 'Erro ao gerar token'], 500);
-        } catch (\Exception $e) {
-            DB::rollBack();
-            return response()->json(['error' => 'Erro no processo de login'], 500);
-        } finally {
-            DB::disconnect();
+        $user = $this->retrieveAdmin();
+        if (!$user) {
+            return response()->json(['error' => 'Email ou senha inválidos'], 400);
         }
+
+        if (!Hash::check($this->password, $user->senha)) {
+            return response()->json(['error' => 'Email ou senha inválidos'], 400);
+        }
+
+        $this->updateLastAccess($user);
+
+        return response()->json([
+            'email' => $user->email,
+            'id' => $user->id,
+            'name' => $user->nome,
+            'lastLogin' => $user->ultimo_acesso->setTimezone('America/Sao_Paulo')->format('Y-m-d\TH:i:sP'),
+            'token' => $this->generateJWT($user->id),
+            'type' => 'pessoa',
+            'key' => 'ADMIN'
+        ]);
     }
 
     private function retrieveAdmin(): ?Pessoa
@@ -64,30 +49,31 @@ readonly class AdminLogin
 
     private function updateLastAccess(Pessoa $user): void
     {
-        $user->timestamps = false;
         $user->ultimo_acesso = now();
         $user->save();
-        $user->timestamps = true;
     }
 
+    /**
+     * @throws RandomException|DateMalformedStringException
+     */
     private function generateJWT(string $userId): string
     {
         $config = Configuration::forSymmetricSigner(
             new Sha256(),
-            InMemory::plainText(config('app.jwt_secret'))
+            InMemory::plainText(env('SECRET_PESSOA'))
         );
 
         $now = new DateTimeImmutable();
-
-        return $config->builder()
-            ->issuedBy(config('app.url'))
-            ->permittedFor(config('app.frontend_url'))
+        $token = $config->builder()
+            ->issuedBy('pixcoin')
+            ->permittedFor('pixcoin_frontend')
             ->identifiedBy(bin2hex(random_bytes(16)))
             ->issuedAt($now)
             ->canOnlyBeUsedAfter($now)
             ->expiresAt($now->modify('+24 hour'))
             ->withClaim('userId', $userId)
-            ->getToken($config->signer(), $config->signingKey())
-            ->toString();
+            ->getToken($config->signer(), $config->signingKey());
+
+        return $token->toString();
     }
 }
