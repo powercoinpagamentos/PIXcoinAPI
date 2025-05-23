706318237454ab08d5035ff9ff3de95b370f64e2
Luiz Filipe da Silva de Jesus <suvilao@gmail.com>
2025-05-23 20:05:12 -0300

    Fecha conexões após processos em GetAllCustomers

--- app/Actions/Customer/GetAllCustomers.php
+++ app/Actions/Customer/GetAllCustomers.php
@@ -5,13 +5,29 @@ namespace App\Actions\Customer;
 use App\Models\Cliente;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 
 readonly class GetAllCustomers
 {
     public function run(): JsonResponse
     {
-        $customers = $this->getCustomers();
-        return response()->json($customers);
+        DB::beginTransaction();
+
+        try {
+            $customers = $this->getCustomers();
+
+            DB::commit();
+
+            return response()->json($customers);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json(
+                ['error' => 'Falha ao recuperar clientes'],
+                500
+            );
+        } finally {
+            DB::disconnect();
+        }
     }
 
     private function getCustomers(): Collection
